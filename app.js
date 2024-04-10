d6d9a18a5a2f8f8c232c233d04b873b4a8a6cbbe
Uranus313 <mehrbodmh82@gmail.com>
2024-04-10 10:55:46 +0330

    using the os module

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const log = require("./logger");
 const path = require("path");
-
+const os = require("os")
 let firstName = "uranus";
 let dirpath = path.parse(__filename);
+let allMemory = os.totalmem();
+let freeMemory = os.freemem();
 // console.log(firstName);
-log.log(dirpath);
+log.log(allMemory);
+log.log(freeMemory);G
