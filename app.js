723f3cc38da492fe5e2d511cd884b126173279b8
Uranus313 <mehrbodmh82@gmail.com>
2024-04-10 11:05:18 +0330

    using the file system module

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const log = require("./logger");
 const path = require("path");
-const os = require("os")
+const os = require("os");
+const fs = require("fs");
 let firstName = "uranus";
 let dirpath = path.parse(__filename);
 let allMemory = os.totalmem();
 let freeMemory = os.freemem();
-// console.log(firstName);
-log.log(allMemory);
-log.log(freeMemory);G
+fs.readdir("./",function(err,files){
+    if(err){
+        console.log("Error",err);
+    }else{
+        console.log("Files",files);
+    }
+});
