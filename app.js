c45edf4140fcf66608ff2f9938fd935fc81925a0
Uranus313 <mehrbodmh82@gmail.com>
2024-04-10 10:31:41 +0330

    using the path module

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let log = require("./logger")
-let firstName = "uranus";
+const log = require("./logger");
+const path = require("path");
 
+let firstName = "uranus";
+let dirpath = path.parse(__filename);
 // console.log(firstName);
-log.log(firstName);
+log.log(dirpath);
