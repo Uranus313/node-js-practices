fac406de0e02fb537a11549bc70845dbe70b3365
Uranus313 <mehrbodmh82@gmail.com>
2024-04-11 22:59:20 +0330

    handling Get requests

--- RESTFULapi/webApp.js
+++ RESTFULapi/webApp.js
@@ -1,16 +1,20 @@
 const express = require("express");
+let posts = [{id : 1, text:"first"},{id : 2, text:"second"},{id : 3, text:"third"}];
 let app = express();
 app.get("/", (req,res) => {
     res.send("hello");
 });
-app.get("/api/numbers", (req,res) => {
-    res.send([1,2,3,4,5]);
+app.get("/api/posts", (req,res) => {
+    res.send(posts);
 });
-app.get("/api/numbers/:id",(req,res) => {
-    res.send(req.params);
-});
-app.get("/api/numbers/:id",(req,res) => {
-    res.send(req.query);
+app.get("/api/posts/:id",(req,res) => {
+    const post = posts.find( post => post.id === parseInt(req.params.id));
+    if (post){
+        res.send(post);
+    }else{
+        res.status(404).send("couldn't find this post");
+    }
+    
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
