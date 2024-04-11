7ff4120477920961d773751ee887f1b4d9f942c5
Uranus313 <mehrbodmh82@gmail.com>
2024-04-11 23:26:53 +0330

    handling post method

--- RESTFULapi/webApp.js
+++ RESTFULapi/webApp.js
@@ -1,6 +1,7 @@
 const express = require("express");
 let posts = [{id : 1, text:"first"},{id : 2, text:"second"},{id : 3, text:"third"}];
 let app = express();
+app.use(express.json());
 app.get("/", (req,res) => {
     res.send("hello");
 });
@@ -15,6 +16,11 @@ app.get("/api/posts/:id",(req,res) => {
         res.status(404).send("couldn't find this post");
     }
     
+});
+app.post("/api/posts",(req,res) =>{
+    const post = {id : posts.length +1 , text : req.body.name};
+    posts.push(post);
+    res.send(post);
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
