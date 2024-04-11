1d7eaaaae5a7941788b40bbcf1ec3ef1fd51de46
Uranus313 <mehrbodmh82@gmail.com>
2024-04-11 22:47:01 +0330

    using parameters and query

--- RESTFULapi/webApp.js
+++ RESTFULapi/webApp.js
@@ -6,6 +6,12 @@ app.get("/", (req,res) => {
 app.get("/api/numbers", (req,res) => {
     res.send([1,2,3,4,5]);
 });
+app.get("/api/numbers/:id",(req,res) => {
+    res.send(req.params);
+});
+app.get("/api/numbers/:id",(req,res) => {
+    res.send(req.query);
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on ${port}...`);
